1812ddc20c2ff3ea108500b44b77044e010a6292
Reeennooo <105305554+Reeennooo@users.noreply.github.com>
2023-12-30 10:49:20 +0300

    Tag Manager

--- src/static/js/gtm.js
+++ src/static/js/gtm.js
@@ -1,3 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-EXRVCEMLLX');
+
+console.log('GTM');
+
 (function (w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
